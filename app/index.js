abd2ba18d6282e61d61299091017ac6603319262
hesquerdo <hesquerdo@uoc.edu>
2021-04-04 18:50:52 +0200

    D&D with Node response if full room

--- app/index.js
+++ app/index.js
@@ -1,91 +1,146 @@
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const util = require('util');
-const { StringDecoder } = require('string_decoder');
-const formidable = require('formidable');
+const http = require("http")
+const fs = require("fs")
+const url = require("url")
+const util = require("util")
+const { StringDecoder } = require("string_decoder")
+const formidable = require("formidable")
 
 //Template
 const userReplace = (user, usersTemplate) => {
-    let output = usersTemplate.replace('{%id%}', user.id);
-    output = output.replace('{%username%}', user.username);
-    output = output.replace('{%image%}', user.image);
+    let output = usersTemplate.replace("{%id%}", user.id)
+    output = output.replace("{%username%}", user.username)
+    output = output.replace("{%image%}", user.image)
 
-    return output;
-};
+    return output
+}
 
-
-const usersTemplate = fs.readFileSync(`${__dirname}/templates/users.html`, 'utf8');
-
+const usersTemplate = fs.readFileSync(
+    `${__dirname}/templates/users.html`,
+    "utf8"
+)
 
 // Data
-const data = fs.readFileSync(`${__dirname}/data/users.json`, 'utf8');
-let dataParse = JSON.parse(data);
+// Users
+const data = fs.readFileSync(`${__dirname}/data/users.json`, "utf8")
+let dataParse = JSON.parse(data)
+
+// Rooms
+const rooms = fs.readFileSync(`${__dirname}/data/rooms.json`, "utf8")
+let roomsParse = JSON.parse(rooms)
 
 // Create the server
 const server = http.createServer((req, res) => {
     // Path
-    const path = req.url;
+    const path = req.url
     // Routing
-    if(path === '/login'){
-        if(req.method === 'POST'){
-
-            let form = new formidable.IncomingForm();
-            form.parse(req, function(err, fields, files){
-                if(err){
-                    console.log(err);
-                    return;
+    if (path === "/login") {
+        if (req.method === "POST") {
+            let form = new formidable.IncomingForm()
+            form.parse(req, function (err, fields, files) {
+                if (err) {
+                    console.log(err)
+                    return
                 }
-               
-                userLogged = fields.user;
-                passLogged = fields.password;
-                         
-                dataParse.forEach(user => {
-                    if(userLogged === user.username && passLogged === user.password){     
-                                                    
-                        res.writeHead(202);
-                   }
-                   if(passLogged !== user.password){
-                        res.writeHead(401);
-                    }
-               
-                })
-              
-               
-        
-            })
-      
-        }
-        const index = fs.readFile(`${__dirname}/templates/login.html`, 'utf8', (error, data)=> {
-            if(error){
-                console.log('Something was wrong!')
-            }else{
-                res.end(data);
-            }
-            
-        })
-       
 
-    }else if(path === '/game'){
-        const usersListTemplate = dataParse.map(user => userReplace(user, usersTemplate)).join('');
-        const index = fs.readFile(`${__dirname}/templates/game.html`, 'utf8', (error, data)=> {
-            if(error){
-                console.log('Something was wrong!')
-            }else{
-                dataView = data.replace('{%USERLIST%}', usersListTemplate);
-                res.end(dataView);
+                userLogged = fields.user
+                passLogged = fields.password
+
+                dataParse.forEach((user) => {
+                    if (
+                        userLogged === user.username &&
+                        passLogged === user.password
+                    ) {
+                        res.writeHead(202)
+                    }
+                    if (passLogged !== user.password) {
+                        res.writeHead(401)
+                    }
+                })
+            })
+        }
+        const index = fs.readFile(
+            `${__dirname}/templates/login.html`,
+            "utf8",
+            (error, data) => {
+                if (error) {
+                    console.log("Something was wrong!")
+                } else {
+                    res.end(data)
+                }
             }
-          
-        })
+        )
+    } else if (path === "/game") {
+        const usersListTemplate = dataParse
+            .map((user) => userReplace(user, usersTemplate))
+            .join("")
+        const index = fs.readFile(
+            `${__dirname}/templates/game.html`,
+            "utf8",
+            (error, data) => {
+                if (error) {
+                    console.log("Something was wrong!")
+                } else {
+                    dataView = data.replace("{%USERLIST%}", usersListTemplate)
+                    res.end(dataView)
+                }
+            }
+        )
+        if (req.method === "POST") {
+            let form = new formidable.IncomingForm()
+            form.parse(req, function (err, fields, files) {
+                if (err) {
+                    console.log(err)
+                    return
+                }
+                roomsParse.forEach((room) => {
+                    if (room.room === fields.room) {
+                        if (room.gamers < 5) {
+                            const currentRoom = roomsParse.filter(
+                                (room) => room.room === fields.room
+                            )
+                            currentGamers = currentRoom[0].gamers
+
+                            const roomModified = {
+                                room: fields.room,
+                                gamers: currentGamers + 1,
+                            }
+                            const otherRooms = roomsParse.filter(
+                                (room) => room.room !== fields.room
+                            )
+                            otherRooms.push(roomModified)
+                            const refreshRooms = JSON.stringify(
+                                otherRooms,
+                                null,
+                                2
+                            )
+
+                            fs.writeFileSync(
+                                `${__dirname}/data/rooms.json`,
+                                refreshRooms,
+                                (error, data) => {
+                                    if (error) {
+                                        console.log("Something was wrong!")
+                                    } else {
+                                        console.log(data)
+                                    }
+                                }
+                            )
+                            res.writeHead(202)
+                        } else {
+                            res.writeHead(401)
+                        }
+                    }
+                })
+            })
+        }
     }
-   
+
     /*else{
         res.writeHead(404);
         res.end('Page not found!')
     }*/
-
 })
 
-server.listen(3001,'127.0.0.1', () => {
-    console.log('server on!');
-});
+server.listen(3001, "127.0.0.1", () => {
+    console.log("server on!")
+})
