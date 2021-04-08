53685f1667b7946ca949570b6bd63ebe25054bce
hesquerdo <hesquerdo@uoc.edu>
2021-04-08 20:17:30 +0200

    Models + Check room + Css

--- app/index.js
+++ app/index.js
@@ -4,36 +4,32 @@ const url = require("url")
 const util = require("util")
 const { StringDecoder } = require("string_decoder")
 const formidable = require("formidable")
+const User = require("./models/user")
+const Game = require("./models/game")
+const Room = require("./models/room")
 
-//Template
-const userReplace = (user, usersTemplate) => {
-    let output = usersTemplate.replace("{%id%}", user.id)
-    output = output.replace("{%username%}", user.username)
-    output = output.replace("{%image%}", user.image)
+let game = new Game()
+var user = new User()
+var roomWind = new Room()
+var roomFire = new Room()
+var roomWater = new Room()
+var roomEarth = new Room()
 
-    return output
-}
+user.setUser("hector", "1111")
+roomWind.setRoom("wind", 0)
+roomFire.setRoom("fire", 2)
+roomWater.setRoom("water", 1)
+roomEarth.setRoom("earth", 0)
 
-const usersTemplate = fs.readFileSync(
-    `${__dirname}/templates/users.html`,
-    "utf8"
-)
-
-// Data
-// Users
-const data = fs.readFileSync(`${__dirname}/data/users.json`, "utf8")
-let dataParse = JSON.parse(data)
-
-// Rooms
-const rooms = fs.readFileSync(`${__dirname}/data/rooms.json`, "utf8")
-let roomsParse = JSON.parse(rooms)
+const allRooms = [roomWind, roomFire, roomWater, roomEarth]
 
 // Create the server
 const server = http.createServer((req, res) => {
     // Path
     const path = req.url
+
     // Routing
-    if (path === "/login") {
+    if (req.url === "/login") {
         if (req.method === "POST") {
             let form = new formidable.IncomingForm()
             form.parse(req, function (err, fields, files) {
@@ -44,18 +40,14 @@ const server = http.createServer((req, res) => {
 
                 userLogged = fields.user
                 passLogged = fields.password
-
-                dataParse.forEach((user) => {
-                    if (
-                        userLogged === user.username &&
-                        passLogged === user.password
-                    ) {
-                        res.writeHead(202)
-                    }
-                    if (passLogged !== user.password) {
-                        res.writeHead(401)
-                    }
-                })
+                if (
+                    user.username === userLogged &&
+                    user.password === passLogged
+                ) {
+                    res.writeHead(202)
+                } else {
+                    res.writeHead(401)
+                }
             })
         }
         const index = fs.readFile(
@@ -70,9 +62,6 @@ const server = http.createServer((req, res) => {
             }
         )
     } else if (path === "/game") {
-        const usersListTemplate = dataParse
-            .map((user) => userReplace(user, usersTemplate))
-            .join("")
         const index = fs.readFile(
             `${__dirname}/templates/game.html`,
             "utf8",
@@ -80,8 +69,7 @@ const server = http.createServer((req, res) => {
                 if (error) {
                     console.log("Something was wrong!")
                 } else {
-                    dataView = data.replace("{%USERLIST%}", usersListTemplate)
-                    res.end(dataView)
+                    res.end(data)
                 }
             }
         )
@@ -92,39 +80,11 @@ const server = http.createServer((req, res) => {
                     console.log(err)
                     return
                 }
-                roomsParse.forEach((room) => {
-                    if (room.room === fields.room) {
-                        if (room.gamers < 5) {
-                            const currentRoom = roomsParse.filter(
-                                (room) => room.room === fields.room
-                            )
-                            currentGamers = currentRoom[0].gamers
 
-                            const roomModified = {
-                                room: fields.room,
-                                gamers: currentGamers + 1,
-                            }
-                            const otherRooms = roomsParse.filter(
-                                (room) => room.room !== fields.room
-                            )
-                            otherRooms.push(roomModified)
-                            const refreshRooms = JSON.stringify(
-                                otherRooms,
-                                null,
-                                2
-                            )
-
-                            fs.writeFileSync(
-                                `${__dirname}/data/rooms.json`,
-                                refreshRooms,
-                                (error, data) => {
-                                    if (error) {
-                                        console.log("Something was wrong!")
-                                    } else {
-                                        console.log(data)
-                                    }
-                                }
-                            )
+                allRooms.forEach((room) => {
+                    if (room.name === fields.roomFor) {
+                        if (room.users < 2) {
+                            game.setGame(room.users, room.name, null)
                             res.writeHead(202)
                         } else {
                             res.writeHead(401)
@@ -133,12 +93,12 @@ const server = http.createServer((req, res) => {
                 })
             })
         }
+    } else if (path === "/endgame") {
+        res.end("End game")
+    } else {
+        res.writeHead(404)
+        res.end("Page not found!")
     }
-
-    /*else{
-        res.writeHead(404);
-        res.end('Page not found!')
-    }*/
 })
 
 server.listen(3001, "127.0.0.1", () => {
