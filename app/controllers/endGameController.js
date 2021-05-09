214a9c0af2a24c5f7b6a26e468995db7ecfff773
hesquerdo <hesquerdo@uoc.edu>
2021-05-09 21:41:22 +0200

    Scores + champion + room reset finished

--- app/controllers/endGameController.js
+++ app/controllers/endGameController.js
@@ -4,65 +4,67 @@ const Room = require("../model/room")
 
 module.exports = {
     post: (req, res) => {
-        const username = req.body.username
+        const username = req.body.champion
         const points = req.body.wins
         const roomFor = req.body.roomFor
 
+        if (username) {
+            Score.findOne({ userName: username }, function (err, score) {
+                if (err) {
+                    console.log(err)
+                    return res.status(500).end()
+                }
+                if (score) {
+                    Score.findOneAndUpdate(
+                        { userName: username },
+                        {
+                            $inc: {
+                                wins: 1,
+                            },
+                        },
+                        function (err, score) {
+                            if (err) {
+                                console.log(err)
+                                return res.status(500).end()
+                            }
+                            if (score) {
+                                res.status(200)
+                                res.end()
+                            }
+                        }
+                    )
+                }
+                if (!score) {
+                    Score.create(
+                        { userName: username, wins: points },
+                        function (err, score) {
+                            if (err) {
+                                console.log(err)
+                                return res.status(500).end()
+                            }
+                            if (score) {
+                                res.status(200)
+                                res.end()
+                            }
+                        }
+                    )
+                }
+            })
+        }
+        console.log(username)
+
         Room.findOneAndUpdate(
             { room: roomFor },
-            { $inc: { users: -1 } },
+            { users: 0 },
             function (err, room) {
                 if (err) {
                     console.log(err)
                 } else {
-                    console.log(
-                        `User: ${username} deleted from room: ${roomFor}`
-                    )
+                    console.log(room.room + "room restarted!")
                 }
             }
         )
 
-        Score.findOne({ userName: username }, function (err, score) {
-            if (err) {
-                console.log(err)
-                return res.status(500).end()
-            }
-            if (score) {
-                Score.findOneAndUpdate(
-                    { userName: username },
-                    {
-                        $inc: {
-                            wins: 1,
-                        },
-                    },
-                    function (err, score) {
-                        if (err) {
-                            console.log(err)
-                            return res.status(500).end()
-                        }
-                        if (score) {
-                            res.status(200)
-                            res.end()
-                        }
-                    }
-                )
-            }
-            if (!score) {
-                Score.create(
-                    { userName: username, wins: points },
-                    function (err, score) {
-                        if (err) {
-                            console.log(err)
-                            return res.status(500).end()
-                        }
-                        if (score) {
-                            res.status(200)
-                            res.end()
-                        }
-                    }
-                )
-            }
-        })
         res.status(200)
         res.end()
     },
